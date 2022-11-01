942af9a5b9a76d864cfc5c4ecc6843622ffeab90
Dzmitry Viartseika <verteyko1990@gmail.com>
2022-11-01 15:03:31 +0100

    task 8

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
-let server: Handler;
+const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.init();
+
   app.enableCors({
     origin: (req, callback) => callback(null, true),
   });
-  await app.init();
+  app.use(helmet());
+
+  await app.listen(port);
   const expressApp = app.getHttpAdapter().getInstance();
   return serverlessExpress({ app: expressApp });
 }
@@ -21,6 +26,6 @@ export const handler: Handler = async (
     context: Context,
     callback: Callback,
 ) => {
-  server = server ?? (await bootstrap());
+  const server = await bootstrap();
   return server(event, context, callback);
 };
